d3993efdd3e6eb09f8ee732cce77308fa2cde90f
Avery Adams <avery.adams@gmail.com>
2019-04-15 21:19:34 -0500

    Initial commit

---
+++ WarriorBookReport/elis_code/elis_javascript.js
@@ -0,0 +1,36 @@
+
+w.setQuestion(
+'What is the title of your book?',
+'The title of my book is warriors #6 the darkest hour');
+w.setQuestion(
+'Who is the author, publisher, And what is the copyright date?',
+'Erin hunter is the author and the publisher is Harper Collins.\n The copyright date is 2004');
+w.setQuestion(
+'Give us a summary.',
+'SPOILER ALERT!!!!!!!   Don’t listen if you\'re reading this series or planning to.\n' +
+'A prophecy from Starclan sends a ominous message to Firestar “Fire alone can save our clan.”-Starclan also Starclan says “Four will become 2 lion and tiger will meet in battle and blood will rule the forest.”-Starclan.  So when Tigerstar tries to get Thunderclan and Windclan to join with Shadowclan and Riverclan Firestar persuades Riverclan and Windclan to join them they become Lionclan when Tigerstar loses all 9 live to Bloodclan Shadowclan joins Lionclan to rid the forest of Scourge and Bloodclan.'
+);
+w.setQuestion(
+ 'Where did it happen and when did it happen?',
+ ' It happened in a forest and the time it happened, I think it was modern day.'
+);
+w.setQuestion(
+   'Who were the main characters?\n',
+ 'The main characters were Firestar, Leopardstar, Tigerstar, Tallstar, and Scourge.'
+);
+w.setQuestion(
+ 'What was the conflict?\n',
+ 'Bloodclan tries to take over the forest.'
+);
+w.setQuestion(
+ 'What was the solution?\n',
+ 'All the clans join together to fight Bloodclan off.'
+);
+w.setQuestion(
+ 'What was the message?',
+ 'Don’t give up and work together.'
+);
+w.setQuestion(
+ 'What do you review the book?',
+ 'I rate this book 6 out of 5 stars and I would recommend it.'
+);
